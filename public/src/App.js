341e20027efa27a0b174014ba56f9532c9250feb
Karaggeorge <karaggeorge0@gmail.com>
2018-04-21 11:33:54 -0400

    First draft

--- public/src/App.js
+++ public/src/App.js
@@ -1,21 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from 'material-ui/Drawer';
+import List from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+import cookies from 'browser-cookies';
+import MainApp from './main';
+
+const url = 'http://localhost:8080/';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  list: {
+    width: 250,
+  },
+};
 
 class App extends Component {
+  state = {
+    isOpen: false,
+    name: ''
+  }
+
+  componentDidMount() {
+    const userId = cookies.get('userId');
+
+    fetch(`${url}user/${userId}`).then(function(response) {
+      return response.json();
+    })
+    .then(function(user) {
+      this.setState(user);
+    });
+  }
+
   render() {
+    const { classes } = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <AppBar position="static">
+        <Toolbar>
+          <Typography variant="title" color="inherit" className={classes.flex}>
+            Title
+          </Typography>
+          {this.state.name}
+        </Toolbar>
+      </AppBar>
+              <MainApp/>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
